e9e12fe5f0d9c2fd79307b91d4272bf0e4fa726f
knecht <ldj910505@naver.com>
2021-08-02 23:17:05 +0900

    <code> 커밋 연습
    * git pull 후 git push 하는 것까지 연습해보겠습니다.

--- AlgorithmStudyCpp/GRADUATION_dbfldkfdbgml.cpp
+++ AlgorithmStudyCpp/GRADUATION_dbfldkfdbgml.cpp
@@ -11,6 +11,7 @@ int main(void) {
 	// git add    : unstaged -> staged
 	// git commit : staged   -> commited
 	// git push   : remote/local 반영
+	// git pull   : remote/local master에서 변경된 커밋들을 fetch + merge = pull
 	cout << "hello world!";
 
 	return 0;
