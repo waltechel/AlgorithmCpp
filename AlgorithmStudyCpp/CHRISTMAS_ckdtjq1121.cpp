b480d81e491a2163bcefea2df99307ef64b502bc
ckdtjq1121 <80ckdtjq@naver.com>
2021-08-13 17:27:59 +0900

    책 보고 다시 풀었습니다

--- AlgorithmStudyCpp/CHRISTMAS_ckdtjq1121.cpp
+++ AlgorithmStudyCpp/CHRISTMAS_ckdtjq1121.cpp
@@ -1,49 +1,72 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
 typedef long long ll;
+const int MAX = 100000;
+const int MOD = 20091101;
 
+ll waysToBuy(const vector<ll>& psum, int K)
+{
+
+	ll ret = 0;
+	vector<ll> count(K, 0);
+	for (int i = 0; i < psum.size(); i++)
+	{
+		count[psum[i]]++;
+	}
+
+	for (int i = 0; i < K; i++)
+	{
+		if (count[i] >= 2)
+			ret = (ret + count[i] * (count[i] - 1) / 2LL) % MOD;
+	}
+
+	return ret;
+}
+
+ll maxBuys(const vector<ll>& psum, int K)
+{
+	vector<ll> ret(psum.size(), 0);
+	vector<ll> prev(K, -1);
+
+	for (int i = 1; i < psum.size(); i++)
+	{
+		ret[i] = ret[i - 1];
+
+		int loc = prev[psum[i]];
+		if (loc != -1)
+			ret[i] = max(ret[i], ret[loc] + 1);
+
+		prev[psum[i]] = i;
+	}
+	return ret.back();
+}
 int main()
 {
 	int t;
 	cin >> t;
 	for (int i = 0; i < t; i++)
 	{
-		int n, k;
-		cin >> n >> k;
-		ll* presentBox = (ll*)malloc(n * sizeof(ll));
-		ll* partSum = (ll*)malloc(n * sizeof(ll));	// 인형의 상자까지의 부분합
-		ll* theRestCount = (ll*)malloc(k * sizeof(ll));	// 나머지의 개수
-		for (int j = 0; j < n; j++)
+		int N, K;
+		cin >> N >> K;
+		if (N < 1 || K < 1 || N > MAX || K > MAX)
+			exit(1);
+
+		vector<ll> v(N + 1);
+		vector<ll> psum(N + 1);
+		psum[0] = 0;
+		ll extra = 0;
+		for (int j = 1; j <= N; j++)
 		{
-			theRestCount[j] = 0;
+			cin >> v[j];
+			psum[j] = (psum[j - 1] + v[j]) % K;
+
+			if (psum[i] == 0)
+				extra++;
 		}
 
-		for (int j = 0; j < n; j++)
-		{
-			cin >> presentBox[j];
-			if (j == 0)
-			{
-				partSum[j] = presentBox[j];
-			}
-			else
-			{
-				partSum[j] = partSum[j - 1] + presentBox[j];
-			}
-
-			partSum[j] = partSum[j] % k;
-
-			theRestCount[partSum[j]]++;
-		}
-
-		ll result_first = 0;
-		ll result_second = -1; // 구현 X
-		for (int j = 0; j < k; j++)
-		{
-			result_first += theRestCount[j] * (theRestCount[j] - 1) / 2;
-			result_first %= 20091101;
-		}
-
-		cout << result_first << " " << result_second;
-
+		cout << (waysToBuy(psum, K) + extra) % MOD << " " << maxBuys(psum, K);
 	}
 }
--- AlgorithmStudyCpp/GRADUATION_ckdtjq1121.cpp
+++ AlgorithmStudyCpp/GRADUATION_ckdtjq1121.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
-
+#include <memory.h>
 using namespace std;
+
+const int MAX = 12;
+const int INF = 987654321;
+int n, k, m, l;
+int cashe[10][(1 << MAX)];
+int classes[10];
+int prerequisite[MAX];
+
+int bitCount(int bits)
+{
+	if (bits == 0)
+		return 0;
+	return bits % 2 + bitCount(bits / 2);
+}
+
+int graduate(int semester, int taken)
+{
+	if (bitCount(taken) >= k) return 0;
+
+	if (semester == m) return INF;
+
+	int& ret = cashe[semester][taken];
+	if (ret != -1) return ret;
+	ret = INF;
+
+	int canTake = (classes[semester] & ~taken);	// taken에 없고 classes에 있는 것들
+	for (int i = 0; i < n; i++)
+	{
+		if ((canTake & (1 << i)) &&
+			(taken & prerequisite[i]) != prerequisite[i])
+			canTake &= ~(1 << i);
+
+
+	}
+
+	for (int take = canTake; take > 0; take = (take - 1) & canTake)
+	{
+		if (bitCount(take) > l) continue;
+		ret = min(ret, graduate(semester + 1, taken | take) + 1);
+	}
+
+	ret = min(ret, graduate(semester + 1, taken));
+	return ret;
+}
 
 int main()
 {
@@ -9,163 +53,46 @@ int main()
 
 	for (int i = 0; i < c; i++)
 	{
-		int resultCount = 0;
-		int n, k, m, l;
+		memset(prerequisite, 0, sizeof(prerequisite));
+		memset(classes, 0, sizeof(classes));
+		memset(cashe, -1, sizeof(cashe));
+
 		cin >> n >> k >> m >> l;
 
-		int endSubjectBit = 0;
-		//bool* endSubject = (bool*)malloc(n * sizeof(bool)); // 이수가 끝난 과목 체크 표
-		//for (int j = 0; j < n; j++)			// false 이수하지 않음으로 초기화
-		//{
-		//	endSubject[j] = false;
-		//}
-		int* preSubjectBit = (int*)malloc(n * sizeof(int));
-		//bool** preSubject = (bool**)malloc(n * sizeof(bool*)); // 선수과목 체크 표
-		for (int j = 0; j < n; j++)			//  false 선수과목이 아님으로 초기화
-		{
-			preSubjectBit[j] = 0;
-			/*for (int a = 0; a < n; a++)
-			{
-				preSubject[j][a] = false;
-			}*/
-
-		}
-
-		int nCount;
-		int temp;
+		int preNum;
+		int subject;
 		for (int j = 0; j < n; j++)		// 선수과목 정보 입력
 		{
-			cin >> nCount;
-			for (int a = 0; a < nCount; a++)
+			cin >> preNum;
+			for (int a = 0; a < preNum; a++)
 			{
-				cin >> temp;
+				cin >> subject;
 
-				preSubjectBit[j] |= (1 << temp);
-				/*preSubject[j][temp] = true;*/
-
+				prerequisite[j] |= (1 << subject);
 			}
 
 		}
 
-		int* semesterBit = (int*)malloc(m * sizeof(int));
-		//bool** semester = (bool**)malloc(m * sizeof(bool*));
-		for (int j = 0; j < m; j++)			//  false 학기에 있는 과목이 아님으로 초기화
-		{
-			semesterBit[j] = 0;
-			/*semester[j] = (bool*)malloc(n * sizeof(bool));
-			for (int a = 0; a < n; a++)
-			{
-				semester[j][a] = false;
-			}*/
-		}
-
-		int mCount;
-
+		int classNum;
 		for (int j = 0; j < m; j++)		// 학기 정보 입력
 		{
-			cin >> mCount;
-			for (int a = 0; a < mCount; a++)
+			cin >> classNum;
+			for (int a = 0; a < classNum; a++)
 			{
-				cin >> temp;
-
-				semesterBit[j] |= (1 << temp);
-				//semester[j][temp] = true;	
-
+				cin >> subject;
+				classes[j] |= (1 << subject);
 			}
-
 		}
 
-		int tempEndSubjectBit = 0;
-		//bool* tempEndSubject = (bool*)malloc(n * sizeof(bool)); // 임시 이수 완료 표
-		//for (int j = 0; j < n; j++)		// false 이수하지 않음으로 초기화
-		//{
-		//	tempEndSubject[j] = false;
-		//}
+		int ret = graduate(0, 0);
 
-		// 학기 시작
-		int result = 0;
-		bool failFlag;
-		for (int j = 0; j < m; j++)	// 학기
-		{
-			for (int a = 0; a < n; a++)		//학기의 과목
-			{
-				//if (!semester[j][a]) // [j]학기에 [a] 과목이 없을 경우 패스
-				if ((semesterBit[j] & (1 << a)) == 0)
-				{
-
-					continue;
-				}
-
-				//if (endSubject[a]) // endSubject[a]를 이미 수강 하였을 경우 패스
-				if (endSubjectBit & (1 << a))
-				{
-					continue;
-				}
-
-				failFlag = false;
-				for (int b = 0; b < n; b++)
-				{
-					//if (preSubject[a][b] && !endSubject[b]) // a의 선수 과목으로 b가 있고 b를 수강 하지 못 했을 때	a를 수강하지 못함
-					if ((preSubjectBit[a] & (1 << b)))
-					{
-						if ((endSubjectBit & (1 << b)) == 0)
-						{
-							failFlag = true;
-							break;
-						}
-					}
-				}
-
-				if (!failFlag)	// 실패하지 않았다면 -> 선수 과목들을 수강완료 했을 경우 수강할 수 있음
-				{
-					//tempEndSubject[a] = true; // 수강 완료
-					tempEndSubjectBit |= (1 << a);
-				}
-			}
-
-
-			// 한 학기 끝
-
-			//for (int a = 0; a < n; a++)	// 전 학기와 다른 점이 있는지?
-			//{
-				//if (endSubject[a] != tempEndSubject[a])	
-			// 
-			if (endSubjectBit != tempEndSubjectBit)
-			{
-				result++;
-				//for (int a = 0; a < n; a++)	// 이번 학기에서 수강 완료한 과목  수강 완료 표에 추가
-				//{
-				//	if (endSubject[a] != tempEndSubject[a])
-				//	{
-				//		endSubject[a] = endSubject[a] || tempEndSubject[a];
-				//	}
-				//}				
-				endSubjectBit |= tempEndSubjectBit;
-			}
-			// 수강완료 과목 개수 카운트
-			resultCount = 0;
-			for (int a = 0; a < n; a++)
-			{
-				//if (endSubject[a])
-				if (endSubjectBit & (1 << a))
-				{
-					resultCount++;
-				}
-			}
-			// 목표 과목 수 이상  수강 완료 ->
-			if (resultCount >= k)
-				break;
-		}
-
-		// 결과 출력
-		if (resultCount < k)
+		if (ret == INF)
 			cout << "IMPOSSIBLE" << "\n";
 		else
-			cout << result << "\n";
+			cout << ret << "\n";
+
 	}
-	// 새로 수강할 수 있는 과목이 있을때 마다 그 학기에서 과목을 수강을 한다. => 최소 학기를 수강하진 않음
-	// 학기 들의 조합 중에 최소 학기가 존재 함  => recursive + dp
-	// 한 학기에 들을 수 있는 과목 수 l(L)로 제한적임
+	return 0;
 }
 
 
