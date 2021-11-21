5d4afd5b13598df5e3674db4251a8779921d41f2
an <adh0463@gmail.com>
2021-11-21 21:13:35 +0900

    20211121 update code

--- AlgorithmStudyCpp/QUADTREE_coloredrabbit.cpp
+++ AlgorithmStudyCpp/QUADTREE_coloredrabbit.cpp
@@ -13,19 +13,43 @@
 #include<cmath>
 using ll = long long;
 using namespace std;
-const int MAX_N = 2e5 + 1, INF = 1e9, SWITCH = 10, SZ=30;
+const int MAX_N = 1001, INF = 1e9, SWITCH=10;
+int pos[MAX_N];
+int divi(char* b, int p, bool _print) {
+	int& ret = pos[p];
+	if (ret == -1) {
+		if (b[p] != 'x')
+			ret = p;
+		else
+			ret = divi(b, divi(b, divi(b, divi(b, p + 1, false) + 1, false) + 1, false) + 1, false);
+	}
+
+	if (_print) {
+		printf("%c", b[p]);
+		if (b[p] == 'x') {
+			int p1 = divi(b, p + 1, false);
+			int p2 = divi(b, p1 + 1, false);
+			int p3 = divi(b, p2 + 1, true);
+			divi(b, p3 + 1, true);
+
+			divi(b, p + 1, true);
+			divi(b, p1 + 1, true);
+		}
+	}
+
+	return ret;
+}
 int main() {
-	freopen("input.txt", "r", stdin);
 	double ans;
-	char s[MAX_N], f[MAX_N];
-	int n, m;
+	char b[MAX_N];
+	int T;
 	scanf("%d\n", &T);
 	while (T--) {
-		scanf("%s\n%s", s, f);
-		n = strlen(s);
-		m = strlen(f);
+		scanf("%s", b);
+		memset(pos, -1, sizeof pos);
 
-
+		divi(b, 0, 1);
+		puts("");
 	}
 	return 0;
 }
