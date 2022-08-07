555f76d85e5cef89d98982a037be13c127bd2bc0
dongjun Lee <ldj910505@gmail.com>
2022-08-07 20:31:33 +0900

    1670 문제 풀이
    - C, D 문제는 못풂

---
+++ AlgorithmStudyCpp/1670A_dbfldkfdbgml.cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+int A[100000];
+int B[100000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    for (int test = 0; test < T; test++) {
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+
+        int negativeCount = 0;
+        for (int i = 0; i < N; i++) {
+            if (A[i] < 0) {
+                negativeCount++;
+            } 
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (negativeCount > 0) {
+                if (A[i] > 0) {
+                    A[i] *= -1;
+                }
+                negativeCount--;
+            } else {
+                if (A[i] < 0) {
+                    A[i] *= -1;
+                }
+            }
+        }
+
+        /*for(int i = 0 ; i < N ; i++){
+            cout << A[i] << " ";
+        }
+        cout << "\n";*/
+
+        bool answer = true;
+        for (int i = 0; i < N - 1; i++) {
+            if (A[i] > A[i + 1]) {
+                answer = false;
+                break;
+            }
+        }
+
+        if (answer) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+
+    }
+
+    return 0;
+}
---
+++ AlgorithmStudyCpp/1670B_dbfldkfdbgml.cpp
@@ -0,0 +1,64 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    for (int test = 0; test < T; test++) {
+        int N;
+        cin >> N;
+
+        string s;
+        cin >> s;
+
+        int K;
+        cin >> K;
+
+        bool names[26];
+        for (int i = 0; i < 26; i++) {
+            names[i] = false;
+        }
+
+        for (int i = 0; i < K; i++) {
+            char t;
+            cin >> t;
+            names[(int)(t - 'a')] = true;
+        }
+
+        for (int i = 0; i < N; i++) {
+            char t = s[i];
+            if (names[(int)(t - 'a')] == true) {
+                s[i] = '1';
+            } else {
+                s[i] = '0';
+            }
+        }
+
+        int answer = 0;
+        for (int i = N - 1; i >= 1; i--) {
+            if (s[i] == '1') {
+                int l = i;
+                for (int j = i - 1; j >= 1; j--) {
+                    if (s[j] == '0') {
+                        l = j;
+                    } else {
+                        break;
+                    }
+                }
+                int length = i - l + 1;
+                answer = max(answer, length);
+            }
+        }
+
+        cout << answer << "\n";
+    }
+
+    return 0;
+}
