d20fdff579e1cbe92c4d4d4be338371f37afc336
dongjun Lee <ldj910505@gmail.com>
2022-10-30 17:38:50 +0900

    abc264 대회 커밋

---
+++ AlgorithmStudyCpp/abc264a_dbfldkfdbgml.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int L, R;
+
+	cin >> L >> R;
+
+	string line = "atcoder";
+	for (int i = L -1 ; i <= R -1 ; i++) {
+		cout << line[i];
+	}
+
+
+	return 0;
+}
+
