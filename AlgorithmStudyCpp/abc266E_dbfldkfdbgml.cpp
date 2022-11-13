80614811f267bc9dfcf957cb3c06863d1113139d
knecht <ldj910505@naver.com>
2022-11-13 18:38:56 +0900

    20221103 스터디 내용 정

---
+++ AlgorithmStudyCpp/abc266E_dbfldkfdbgml.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.precision(10);
+
+
+	double* dp = new double[101];
+	dp[1] = 3.5;
+	for (int i = 2; i <= 100; i++) {
+		dp[i] = max(dp[i - 1], 1.0) / 6.0
+			+ max(dp[i - 1], 2.0) / 6.0
+			+ max(dp[i - 1], 3.0) / 6.0
+			+ max(dp[i - 1], 4.0) / 6.0
+			+ max(dp[i - 1], 5.0) / 6.0
+			+ max(dp[i - 1], 6.0) / 6.0;
+	}
+
+	int N;
+	cin >> N;
+	cout << dp[N];
+
+	return 0;
+}
+
---
+++ AlgorithmStudyCpp/abc266a_dbfldkfdbgml.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+
+using namespace std;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string line;
+	cin >> line;
+
+	cout << line[line.length() / 2];
+
+	return 0;
+}
