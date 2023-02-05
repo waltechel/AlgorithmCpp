2316b6e763229b3604ce227ed64cd991e037b60e
knecht <ldj910505@naver.com>
2023-02-05 20:56:49 +0900

    다른 경로로 최단이 나오면 그냥 없애면 된다

--- AlgorithmStudyCpp/abc243e_dbfldkfdbgml.cpp
+++ AlgorithmStudyCpp/abc243e_dbfldkfdbgml.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 /*
 	플로이드 워셜의 최단 경로를 만족하는 간선을 유지시키면 된다.
-	이런 문제가 진짜 제일 좋은듯
 */
 
 int main() {
@@ -27,9 +26,8 @@ int main() {
 		int* edge = new int[3];
 		cin >> edge[0] >> edge[1] >> edge[2];
 		edgeList.push_back(edge);
-		// 사실 쓸 데 없는 조건이었음
-		graph[edge[0]][edge[1]] = min(graph[edge[0]][edge[1]], (long long) edge[2]);
-		graph[edge[1]][edge[0]] = min(graph[edge[1]][edge[0]], (long long) edge[2]);
+		graph[edge[0]][edge[1]] = min(graph[edge[0]][edge[1]], (long long)edge[2]);
+		graph[edge[1]][edge[0]] = min(graph[edge[1]][edge[0]], (long long)edge[2]);
 	}
 
 	for (int k = 1; k <= N; k++) {
@@ -40,12 +38,22 @@ int main() {
 		}
 	}
 
-	// 이게 왜 답이 안되는지는 모르겠어요
+	// 이렇게 하면 더 비싼 간선을 제외할 수는 있음
 	int answer = 0;
+	//for (int i = 0; i < M; i++) {
+	//	int* edge = edgeList[i];
+	//	if (edge[2] > graph[edge[0]][edge[1]]) {
+	//		answer++;
+	//	}
+	//}
+
 	for (int i = 0; i < M; i++) {
 		int* edge = edgeList[i];
-		if (edge[2] > graph[edge[0]][edge[1]]) {
-			answer++;
+		for (int j = 1; j <= N; j++) {
+			if (edge[2] >= graph[edge[0]][j] + graph[j][edge[1]]) {
+				answer++;
+				break;
+			}
 		}
 	}
 
