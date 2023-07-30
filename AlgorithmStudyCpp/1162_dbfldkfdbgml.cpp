9145c1c101f22e7c84b72db265888d313328781f
dongjun Lee <ldj910505@gmail.com>
2023-07-30 16:56:06 +0900

    백준 문제 두개 품
    - 구현이 생각보다 어렵네
    - a, c는 문제가 사실 비슷해서 대충 비벼봄

---
+++ AlgorithmStudyCpp/1162_dbfldkfdbgml.cpp
@@ -0,0 +1,105 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+#include<regex>
+
+using namespace std;
+
+/*
+	2차원 dp로 풀림
+	dp[i][k] i 번째 정점에 k번의 도로포장을 해서 이동한 최소 거리
+*/
+
+struct comparator {
+	bool operator()(const long long* a, const long long* b) const {
+		return a[2] > b[2]; 
+	}
+};
+
+vector<pair<int, int>> graph[10001];
+long long dp[10001][21];
+
+long long const MAX = 1000000000000000l;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M, K;
+	cin >> N >> M >> K;
+
+	for (int i = 0; i < M; i++) {
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		graph[from].push_back({ to, cost });
+		graph[to].push_back({ from, cost });
+	}
+
+	for (int i = 0; i <= 10000; i++) {
+		for (int j = 0; j <= 20; j++) {
+			dp[i][j] = MAX;
+		}
+	}
+
+	priority_queue<long long*, vector<long long*>, comparator> que;
+	long long* start = new long long[3];
+	start[0] = 1;
+	start[1] = 0;
+	dp[start[0]][start[1]] = 0;
+	start[2] = dp[start[0]][start[1]];
+
+	que.push(start);
+
+	while (!que.empty()) {
+		long long* now = que.top();
+		que.pop();
+		
+		// 너무 커지면 무조건 줄여버림
+		if (dp[now[0]][now[1]] < now[2]) {
+			continue;
+		}
+
+		for (pair<int, int> edge : graph[now[0]]) {
+			int next = edge.first;
+			int cost = edge.second;
+
+			// 도로포장을 안 쓰고 갈 수 있는지를 확인한다
+			if (dp[next][now[1]] > dp[now[0]][now[1]] + cost) {
+				dp[next][now[1]] = dp[now[0]][now[1]] + cost;
+				long long* newPoint = new long long[3];
+				newPoint[0] = next;
+				newPoint[1] = now[1];
+				newPoint[2] = dp[next][now[1]];
+				que.push(newPoint);
+			}
+
+			// 도로포장을 쓰면 갈 수 있는지를 확인한다
+			if (now[1] + 1 <= K && dp[next][now[1] + 1] > dp[now[0]][now[1]]) {
+				dp[next][now[1] + 1] = dp[now[0]][now[1]];
+				long long* newPoint = new long long[3];
+				newPoint[0] = next;
+				newPoint[1] = now[1] + 1;
+				newPoint[2] = dp[next][now[1] + 1];
+				que.push(newPoint);
+			}
+		}
+
+	}
+
+	long long answer = MAX;
+	for (int i = 0; i <= K; i++) {
+		answer = min(answer, dp[N][i]);
+	}
+
+	cout << answer;
+
+	return 0;
+}
---
+++ AlgorithmStudyCpp/16933_dbfldkfdbgml.cpp
@@ -0,0 +1,135 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<unordered_set>
+#include<regex>
+
+using namespace std;
+
+/*
+	3차원으로 풀림
+	graph[i][j][k] - i, j 곳을 k번만에 깨서 갈 수 있어야 한다
+	이 문제의 특징은 깰 수 있는 곳은 반드시 짝수 번에 가서 깨야 한다
+	만약에 현재 위치가 짝수(밤)이면 +1이 아니라 +2를 더해서 깨야 함
+*/
+
+vector<vector<int>> DIRECTIONS = { {-1, 0}, {1, 0}, {0, 1 }, {0, -1} };
+
+int dp[1001][1001][11];
+char graph[1001][1001];
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int R, C, K;
+	cin >> R >> C >> K;
+
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			cin >> graph[i][j];
+		}
+	}
+
+	/*cout << "graph 검증\n";
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}*/
+
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			for (int k = 0; k <= 10; k++) {
+				dp[i][j][k] = 20000000;
+			}
+		}
+	}
+
+	queue<int*> que;
+	dp[1][1][0] = 1;
+	int* start = new int[4];
+	start[0] = 1; // i
+	start[1] = 1; // j
+	start[2] = 0; // k
+	start[3] = 1; // depth
+	que.push(start);
+
+	while (!que.empty()) {
+		int* now = que.front();
+		que.pop();
+
+		//cout << "now is " << now[0] << ", " << now[1] << ", " << now[2] << ", " << now[3] << "\n";
+
+		for (int d = 0; d < DIRECTIONS.size(); d++) {
+			int newRow = now[0] + DIRECTIONS[d][0];
+			int newCol = now[1] + DIRECTIONS[d][1];
+			
+			// 빈 곳이다
+			if (newRow >= 1 && newRow <= R && newCol >= 1 && newCol <= C && graph[newRow][newCol] == '0') {
+
+				//cout << "dp[" << newRow << "][" << newCol << "][" << now[2] << "] is : " << dp[newRow][newCol][now[2]] << ", dp[" << now[0] << "][" << now[1] << "][" << now[2] << "] + 1 is : " << dp[now[0]][now[1]][now[2]] + 1 << "\n";
+
+				if (dp[newRow][newCol][now[2]] > dp[now[0]][now[1]][now[2]] + 1) {
+					dp[newRow][newCol][now[2]] = dp[now[0]][now[1]][now[2]] + 1;
+					int* next = new int[4];
+					next[0] = newRow;
+					next[1] = newCol;
+					next[2] = now[2];
+					next[3] = dp[newRow][newCol][now[2]];
+					que.push(next);
+				}
+			}
+
+			// 벽이다
+			if (newRow >= 1 && newRow <= R && newCol >= 1 && newCol <= C && graph[newRow][newCol] == '1' && now[2] + 1 <= K) {
+
+				//cout << "dp[" << newRow << "][" << newCol << "][" << now[2] << "] is : " << dp[newRow][newCol][now[2]] << ", dp[" << now[0] << "][" << now[1] << "][" << now[2] << "] + 1 is : " << dp[now[0]][now[1]][now[2]] + 1 << "\n";
+
+				// 낮일 경우는 바로 돌파 가능
+				if (dp[now[0]][now[1]][now[2]] % 2 == 1 && dp[newRow][newCol][now[2] + 1] > dp[now[0]][now[1]][now[2]] + 1) {
+					dp[newRow][newCol][now[2] + 1] = dp[now[0]][now[1]][now[2]] + 1;
+					int* next = new int[4];
+					next[0] = newRow;
+					next[1] = newCol;
+					next[2] = now[2] + 1;
+					next[3] = dp[newRow][newCol][now[2] + 1];
+					que.push(next);
+				}
+
+				// 밤일 경우는 기다렸다가 돌파
+				if (dp[now[0]][now[1]][now[2]] % 2 == 0 && dp[newRow][newCol][now[2] + 1] > dp[now[0]][now[1]][now[2]] + 2) {
+					dp[newRow][newCol][now[2] + 1] = dp[now[0]][now[1]][now[2]] + 2;
+					int* next = new int[4];
+					next[0] = newRow;
+					next[1] = newCol;
+					next[2] = now[2] + 1;
+					next[3] = dp[newRow][newCol][now[2] + 1];
+					que.push(next);
+				}
+			}
+		}
+	}
+
+	// 이렇게 했을 때 맨 마지막 위치의 최솟값을 찾으면 정답이 나옴
+	int answer = 20000000;
+	for (int i = 0; i <= K; i++) {
+		answer = min(answer, dp[R][C][i]);
+	}
+	if (answer == 20000000) {
+		cout << "-1";
+	} else {
+		cout << answer;
+	}
+
+
+	return 0;
+}
