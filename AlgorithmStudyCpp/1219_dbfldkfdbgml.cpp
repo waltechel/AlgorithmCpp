f575873ec9f905753ce14a87dcee375016eb3acb
knecht <ldj910505@naver.com>
2023-08-20 20:56:37 +0900

    알고리즘 문제 풀이 소스
    - 두 문제 모두 성공하지 못한 풀이

---
+++ AlgorithmStudyCpp/1219_dbfldkfdbgml.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <regex>
+#include <chrono> // 추가
+
+using namespace std;
+using namespace chrono; // 추가
+
+/*
+    플로이드 워셜 하되, 그 지점에서는 돈을 받으니까 cost를 빼면 된다.
+*/
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, start, end, M;
+    cin >> N >> start >> end >> M;
+
+    vector<int*> edgeList;
+    for (int i = 0; i < M; i++) {
+        int* temp = new int[3];
+        cin >> temp[0] >> temp[1] >> temp[2];
+        edgeList.push_back(temp);
+    }
+
+    int* moneyList = new int[N];
+    for (int i = 0; i < N; i++) {
+        cin >> moneyList[i];
+    }
+
+    for (int* edge : edgeList) {
+        int to = edge[1];
+        int money = moneyList[to];
+        edge[2] -= money;
+    }
+
+    long long MAX = (long long)10000000000000000l;
+    long long** graph = new long long* [N];
+    for (int i = 0; i < N; i++) {
+        graph[i] = new long long[N];
+        for (int j = 0; j < N; j++) {
+            graph[i][j] = MAX;
+        }
+    }
+    bool** canGo = new bool* [N];
+    for (int i = 0; i < N; i++) {
+        canGo[i] = new bool[N];
+        for (int j = 0; j < N; j++) {
+            canGo[i][j] = false;
+        }
+    }
+
+    for (int* edge : edgeList) {
+        int from = edge[0];
+        int to = edge[1];
+        int cost = edge[2];
+        canGo[from][to] = true;
+        graph[from][to] = min(graph[from][to], (long long)cost);
+    }
+
+    // 플로이드
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (canGo[i][k] && canGo[k][j]) {
+                    canGo[i][j] = true;
+                    graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+    }
+
+    // 만약에 도착 도시에 도착하는 것이 불가능한 경우 gg 출력
+    if (canGo[start][end] == false) {
+        cout << "gg";
+        return 0;
+    }
+
+    // 만약에 음수 사이클이 존재하는 경우 "Gee" 출력
+    bool isCycle = false;
+    for (int i = 0; i < N; i++) {
+        if (canGo[start][i] && canGo[i][end] && canGo[i][i] && graph[i][i] < 0) {
+            isCycle = true;
+        }
+    }
+
+    if (isCycle || (start == end && canGo[start][end] && graph[start][end] < 0)) {
+        cout << "Gee";
+        return 0;
+    }
+
+    //// 만약에 시작 도시와 도착 도시가 같고, 도착 도시에 이미 도착했다면 금액만 갖고 끝
+    if (start == end) {
+        if (canGo[start][end] && graph[start][end] < 0) {
+            cout << "Gee";
+        } else {
+            cout << moneyList[start];
+        }
+        return 0;
+    }
+
+    // 그게 아니면 비용에 시작한 곳에서의 금액 출력
+    cout << graph[start][end] * -1 + moneyList[start];
+
+    return 0;
+}
---
+++ AlgorithmStudyCpp/1981_dbfldkfdbgml.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <regex>
+#include <chrono> // 추가
+
+using namespace std;
+using namespace chrono; // 추가
+
+vector<vector<int>> DIRECTIONS = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+
+/*
+    희한하게 이분탐색을 안 하면 4초 정도 나온다
+    이분탐색을 하면 시간이 줄어든다.
+*/
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<vector<int>> graph(N, vector<int>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    auto start_time = high_resolution_clock::now(); // 시작 시간 측정
+    int answer = -1;
+    for (int diff = 128; diff > 0; diff /= 2) {
+        answer += diff;
+        vector<vector<vector<bool>>> visited(N, vector<vector<bool>>(N, vector<bool>(201, false)));
+
+        queue<int*> que;
+        int* temp = new int[4];
+        temp[0] = 0;
+        temp[1] = 0;
+        temp[2] = graph[0][0]; // 최솟값
+        temp[3] = graph[0][0]; // 최댓값
+        que.push(temp);
+
+        while (!que.empty()) {
+            int* now = que.front();
+            que.pop();
+            int row = now[0];
+            int col = now[1];
+            for (int d = 0; d < DIRECTIONS.size(); d++) {
+                int newRow = row + DIRECTIONS[d][0];
+                int newCol = col + DIRECTIONS[d][1];
+                if (newRow >= 0 && newRow < N && newCol < N && newCol >= 0) {
+                    int nextVal = graph[newRow][newCol];
+                    int nextMin = min(now[2], nextVal);
+                    int nextMax = max(now[3], nextVal);
+                    // 사전에 허용 가능한 범위를 넘어서면 초과해야 함
+                    if (nextMax - nextMin > answer) {
+                        continue;
+                    }
+
+                    // 이전에 한번 간 적이 있는 범위라면 가지 말아야 함
+                    if (visited[newRow][newCol][nextMax - nextMin]) {
+                        continue;
+                    }
+                    // 그게 아니면 map에 집어넣고 queue에 넣음
+                    visited[newRow][newCol][nextMax - nextMin] = true;
+                    int* next = new int[4];
+                    next[0] = newRow;
+                    next[1] = newCol;
+                    next[2] = nextMin;
+                    next[3] = nextMax;
+                    que.push(next);
+                }
+            }
+        }
+
+        bool find = false;
+        for (int i = 0; i <= answer; i++) {
+            if (visited[N - 1][N - 1][i]) {
+                find = true;
+                break;
+            }
+        }
+        if (find) {
+            answer -= diff;
+        }         
+    }
+
+    cout << answer + 1 << "";
+
+    auto end_time = high_resolution_clock::now(); // 종료 시간 측정
+    auto duration = duration_cast<milliseconds>(end_time - start_time); // 실행 시간 계산
+
+    //cout << "\nExecution Time: " << duration.count() << " ms\n"; // 실행 시간 출력
+
+    return 0;
+}
